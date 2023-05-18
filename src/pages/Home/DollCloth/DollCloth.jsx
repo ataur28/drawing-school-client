98460b1425a4ed8a9e8543080de30b8179b6dd2b
ataur28 <ataurrahman862828@gmail.com>
2023-05-19 02:56:56 +0600

    added extra one section

--- src/pages/Home/DollCloth/DollCloth.jsx
+++ src/pages/Home/DollCloth/DollCloth.jsx
@@ -1,11 +1,17 @@
 import dollCloth1 from '../../../assets/images/dollCloth/dollCloth1.png'
+import dollCloth2 from '../../../assets/images/dollCloth/dollCloth2.png'
+import dollCloth3 from '../../../assets/images/dollCloth/dollCloth3.png'
+import dollCloth4 from '../../../assets/images/dollCloth/dollCloth4.png'
 
 const DollCloth = () => {
     return (
         <div>
-            <h2 className="text-4xl font-bold pt-8 text-center text-gray-600">Cloth and accessories for your dolls</h2>
-            <div>
-                <img src="" alt="" />
+            <h2 className="text-4xl font-bold pt-8 text-center text-gray-600 mb-6">Cloth and accessories for your dolls</h2>
+            <div className='lg:flex justify-between'>
+                <img className='hover:scale-125 transition ease-in-out duration-300' src={dollCloth1} alt="" />
+                <img className='hover:scale-125 transition ease-in-out duration-300' src={dollCloth2} alt="" />
+                <img className='hover:scale-125 transition ease-in-out duration-300' src={dollCloth3} alt="" />
+                <img className='hover:scale-125 transition ease-in-out duration-300' src={dollCloth4} alt="" />
             </div>
         </div>
     );

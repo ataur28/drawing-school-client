a746016ce7e6121c617973e0477073f20694c087
ataur28 <ataurrahman862828@gmail.com>
2023-05-19 02:43:08 +0600

    added footer section

---
+++ src/pages/Home/DollCloth/DollCloth.jsx
@@ -0,0 +1,14 @@
+import dollCloth1 from '../../../assets/images/dollCloth/dollCloth1.png'
+
+const DollCloth = () => {
+    return (
+        <div>
+            <h2 className="text-4xl font-bold pt-8 text-center text-gray-600">Cloth and accessories for your dolls</h2>
+            <div>
+                <img src="" alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default DollCloth;
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,10 +1,14 @@
 import Banner from "../Banner/Banner";
+import DollCloth from "../DollCloth/DollCloth";
+import UserReview from "../UserReview/UserReview";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <h2>this is home</h2>
+            <DollCloth></DollCloth>
+            <UserReview></UserReview>
         </div>
     );
 };

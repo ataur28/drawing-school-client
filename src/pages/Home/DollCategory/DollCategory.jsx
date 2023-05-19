e1a7f2bcd31cb1fbfb5aa97df5b819fdfefdce30
ataur28 <ataurrahman862828@gmail.com>
2023-05-19 13:56:30 +0600

    added react tab condition

--- src/pages/Home/DollCategory/DollCategory.jsx
+++ src/pages/Home/DollCategory/DollCategory.jsx
@@ -1,21 +1,81 @@
+import { useEffect, useState } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
+import SubCategory from '../SubCategory/SubCategory';
 
 const DollCategory = () => {
+    const [dolls, setDolls] = useState([]);
+    const [activeTab, setActiveTab] = useState("baby");
+    
+
+    useEffect(() => {
+        fetch('dolls.json')
+            .then(res => res.json())
+            .then(data => setDolls(data))
+    }, [])
+
+    const result = dolls.filter((doll) => doll.category == activeTab);
+    // console.log(result);
+
+    const handleTabClick = (tabName) => {
+        setActiveTab(tabName);
+      };
     return (
         <div className='mt-10 mb-6'>
             <h2 className='text-4xl font-bold mb-8 text-center text-gray-600'>Doll Category</h2>
+
+            <p>dolls {dolls.length}</p>
+
+
+            {/* <div>
+                {
+                    dolls?.map(subDolls => <SubCategory
+                        key={subDolls._id}
+                        subDolls={subDolls}
+                    ></SubCategory>)
+                }
+            </div> */}
+
+
             <Tabs className="text-center">
+
                 <TabList>
-                    <Tab>Title 1</Tab>
-                    <Tab>Title 2</Tab>
+                    <Tab onClick={() => handleTabClick("baby")}>Bady Dolls</Tab>
+                    <Tab onClick={() => handleTabClick("barbie")}>Barbie Dolls</Tab>
+                    <Tab onClick={() => handleTabClick("american")}>American Dolls</Tab>
+                    {/* <Tab >Bady Dolls</Tab> */}
+                    {/* <Tab >Barbie Dolls</Tab>
+                    <Tab >American Dolls</Tab> */}
                 </TabList>
 
                 <TabPanel>
                     <h2>Any content 1</h2>
+                    <div>
+                    {
+                        result?.map(subDolls => <SubCategory
+                        key={subDolls._id}
+                        subDolls={subDolls}
+                        ></SubCategory>)
+                    }
+                    </div>
                 </TabPanel>
                 <TabPanel>
                     <h2>Any content 2</h2>
+                    {
+                        result?.map(subDolls => <SubCategory
+                        key={subDolls._id}
+                        subDolls={subDolls}
+                        ></SubCategory>)
+                    }
+                </TabPanel>
+                <TabPanel>
+                    <h2>Any content 3</h2>
+                    {
+                        result?.map(subDolls => <SubCategory
+                        key={subDolls._id}
+                        subDolls={subDolls}
+                        ></SubCategory>)
+                    }
                 </TabPanel>
             </Tabs>
         </div>
---
+++ src/pages/Home/SubCategory/SubCategory.jsx
@@ -0,0 +1,13 @@
+
+
+const SubCategory = ({subDolls}) => {
+    // console.log(subDolls);
+    const {picture, toyName, price, rating, category} = subDolls;
+    return (
+        <div>
+            <h1>subbbbbbbbb {category}</h1>
+        </div>
+    );
+};
+
+export default SubCategory;

9c710f2f0e12434f0556b82776d1c78fb07cc646
ataur28 <ataurrahman862828@gmail.com>
2023-06-16 01:36:30 +0600

    added email password validation

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -9,32 +9,50 @@ import Swal from "sweetalert2";
 
 const Login = () => {
     useTitle('Login');
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState('');
 
-    const {signIn} = useContext(AuthContext)
+    const { signIn } = useContext(AuthContext)
 
     const [user, setUser] = useState(null);
     const auth = getAuth(app);
     //  console.log(app)
     const provider = new GoogleAuthProvider();
 
-    
+
     const navigate = useNavigate();
     const location = useLocation();
     // console.log('login page location', location)
     const from = location.state?.from?.pathname || '/'
 
-    const handleLogin = event =>{
+    const handleLogin = event => {
         event.preventDefault();
+        setSuccess('');
+        setError('');
         const form = event.target;
         const email = form.email.value;
         const password = form.password.value;
         console.log(email, password);
 
+        // validate
+        if (!/(?=.*[A-Z])/.test(password)) {
+            setError('Incorrect Password');
+            return;
+        }
+        else if (!/(?=.*[0-9].*[0-9])/.test(password)) {
+            setError('Incorrect Password');
+            return
+        }
+        else if (password.length < 6) {
+            setError('Incorrect Password')
+            return;
+        }
+
         signIn(email, password)
             .then(result => {
                 const loggedUser = result.user;
                 console.log(loggedUser);
-                
+
                 // alert("User login successful")
                 Swal.fire('User login successful')
                 navigate(from, { replace: true })
@@ -56,7 +74,7 @@ const Login = () => {
                 setUser(LoggedInUser);
                 // alert("User login successful")
                 Swal.fire('User login successful')
-                
+
             })
             .catch(error => {
                 console.log('error', error.message)
@@ -66,7 +84,7 @@ const Login = () => {
     return (
         <div>
             <div className="hero min-h-[600px] bg-base-100">
-                
+
 
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                     <div className="card-body">
@@ -76,17 +94,17 @@ const Login = () => {
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="text" 
-                                name="email"
-                                placeholder="email" className="input input-bordered" />
+                                <input type="text"
+                                    name="email"
+                                    placeholder="email" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="password" 
-                                name="password"
-                                placeholder="password" className="input input-bordered" />
+                                <input type="password"
+                                    name="password"
+                                    placeholder="password" className="input input-bordered" required />
                                 <label className="label">
                                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                 </label>
@@ -97,6 +115,10 @@ const Login = () => {
                             </div>
                         </form>
                         <p className="my-4 text-center">New to Doll House?  <Link className="text-orange-600 font-bold" to="/signUp">SignUp</Link></p>
+                    </div>
+                    <div className="mt-4 mb-10 text-center">
+                        <p className='text-red-600 font-bold'>{error}</p>
+                        <p className='text-green-600 font-bold'>{success}</p>
                     </div>
                 </div>
                 {/* </div> */}
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,23 +1,40 @@
 
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../provider/AuthProvider";
 import useTitle from "../../hook/useTitle";
 import Swal from "sweetalert2";
 const SignUp = () => {
     useTitle('SignUp');
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState('');
 
-    const {createUser} = useContext(AuthContext);
+    const { createUser } = useContext(AuthContext);
 
-    const handleSignUp = event =>{
+    const handleSignUp = event => {
         event.preventDefault();
+        setSuccess('');
+        setError('');
         const form = event.target;
         const name = form.name.value;
         const email = form.email.value;
         const password = form.password.value;
         const confirmPassword = form.confirmPassword.value;
         const photo = form.photo.value;
-        console.log(name, email, password, confirmPassword,  photo);
+        console.log(name, email, password, confirmPassword, photo);
+        // validate
+        if (!/(?=.*[A-Z])/.test(password)) {
+            setError('Please add at least one uppercase in password filed');
+            return;
+        }
+        else if (!/(?=.*[0-9].*[0-9])/.test(password)) {
+            setError('Please add at least two numbers in password filed');
+            return
+        }
+        else if (password.length < 6) {
+            setError('Please add at least 6 characters in your password in password filed')
+            return;
+        }
 
         // createUser(email, password)
         //     .then(result => {
@@ -31,28 +48,28 @@ const SignUp = () => {
         //         console.log(error);
         //     })
 
-        if (password == confirmPassword){
+        if (password == confirmPassword) {
             createUser(email, password)
-            .then(result => {
-                const createdUser = result.user;
-                // setSuccess('User Register successfully');
-                // navigate(from, { replace: true })
-                // alert("User Register successfully")
-                Swal.fire('User Register successfully')
-                console.log(createdUser);
-            })
-            .catch(error => {
-                console.log(error);
-            })
+                .then(result => {
+                    const createdUser = result.user;
+                    // setSuccess('User Register successfully');
+                    // navigate(from, { replace: true })
+                    // alert("User Register successfully")
+                    Swal.fire('User Register successfully')
+                    console.log(createdUser);
+                })
+                .catch(error => {
+                    console.log(error);
+                })
         }
-        else{
+        else {
             alert("Your password and confirm password is not same.Please give correct password.")
         }
 
     }
     return (
         <div>
-            
+
             <div className="hero min-h-[700px] bg-base-100">
 
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
@@ -63,43 +80,43 @@ const SignUp = () => {
                                 <label className="label">
                                     <span className="label-text">Name</span>
                                 </label>
-                                <input type="text" 
-                                name="name"
-                                placeholder="name" className="input input-bordered" />
+                                <input type="text"
+                                    name="name"
+                                    placeholder="name" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="text" 
-                                name="email"
-                                placeholder="email" className="input input-bordered" />
+                                <input type="text"
+                                    name="email"
+                                    placeholder="email" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="password" 
-                                name="password"
-                                placeholder="password" className="input input-bordered" />
-                                
+                                <input type="password"
+                                    name="password"
+                                    placeholder="password" className="input input-bordered" required />
+
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Confirm Password</span>
                                 </label>
-                                <input type="password" 
-                                name="confirmPassword"
-                                placeholder="password" className="input input-bordered" />
-                                
+                                <input type="password"
+                                    name="confirmPassword"
+                                    placeholder="password" className="input input-bordered" required />
+
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Photo URL</span>
                                 </label>
-                                <input type="text" 
-                                name="photo"
-                                placeholder="photo url" className="input input-bordered" />
+                                <input type="text"
+                                    name="photo"
+                                    placeholder="photo url" className="input input-bordered" required />
                                 <label className="label">
                                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                 </label>
@@ -109,6 +126,10 @@ const SignUp = () => {
                             </div>
                         </form>
                         <p className="my-4 text-center">Already Have an Account?  <Link className="text-orange-600 font-bold" to="/login">Login</Link></p>
+                    </div>
+                    <div className="mb-5 text-center">
+                        <p className='text-red-600 font-bold'>{error}</p>
+                        <p className='text-green-600 font-bold'>{success}</p>
                     </div>
                 </div>
                 {/* </div> */}
